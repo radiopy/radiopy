56d1593bb60c3fff556d61a6fc12f41d083b3fbb
Niko <github@dieserniko.link>
2024-11-24 16:29:04 +0100

    Support for config inside environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 trap "service cron stop && exit 0" SIGTERM
 
+# Replace config.toml if RADIOPY_CONFIG is set
+if [ -n "$RADIOPY_CONFIG" ]; then
+    echo "$RADIOPY_CONFIG" > /app/config.toml
+fi
+
+
 # truncate the log file before tailing it
 > /app/radiopy.log
 tail -f /app/radiopy.log &
